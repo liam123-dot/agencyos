8bfe1d4c14a1169289e24142a176c201e595c16e
Liam Buchanan <l.buchanan358@gmail.com>
2025-09-24 13:46:43 +0100

    allow supabase object storage

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
+const next_public_supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL!
+
+// const next host
+const next_public_supabase_host = next_public_supabase_url.replace('http://', '').replace('https://', '').replace('://', '').replace('www.', '').replace('//', '')
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
@@ -8,6 +14,11 @@ const nextConfig: NextConfig = {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '54321',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'http',
+        hostname: next_public_supabase_host,
         pathname: '/storage/v1/object/public/**',
       }
     ]
